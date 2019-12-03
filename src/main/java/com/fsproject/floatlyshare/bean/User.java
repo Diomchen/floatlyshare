c83ee2f80689f61a00c808fede50f1d30895bfe6
[bits/stdc++.h] <153583852@qq.com>
2019-12-03 14:34:48 +0800

    2019/12/3

--- src/main/java/com/fsproject/floatlyshare/bean/User.java
+++ src/main/java/com/fsproject/floatlyshare/bean/User.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 
@@ -19,6 +21,9 @@ public class User extends Model<User> {
     private String nickname;
     private String mail;
     private String password;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+
 
     @Override
     protected Serializable pkVal() {
@@ -30,5 +35,6 @@ public class User extends Model<User> {
         this.mail = mail;
         this.password = password;
     }
+
 
 }
